d74765f2e23b658cebaf766fd6fe08bf6785c7eb
ThiagoFPMR <thiagofpmr@ufrn.edu.br>
2020-11-26 18:37:28 -0300

    small fix

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
+import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 colors = {
     'background': '#FFFFFF',
